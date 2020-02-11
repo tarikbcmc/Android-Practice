cb306a383ba888e35de74bbaf9b923e5d3fabdb8
Tarik Billa <tarikbillabcmc@gmail.com>
2020-02-11 16:23:35 +0600

    Create activitys, Click, Toast
    Today We Learn create registration & login  Activity and click

---
+++ 11-02-2020/java/com/bcmc/practice10/Login.java
@@ -0,0 +1,14 @@
+package com.bcmc.practice10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ 11-02-2020/java/com/bcmc/practice10/MainActivity.java
@@ -0,0 +1,42 @@
+package com.bcmc.practice10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button login_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login_btn =(Button)findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Click-",Toast.LENGTH_LONG).show();
+            }
+        });
+
+//       click registration button
+        findViewById(R.id.reg_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                startActivity(new Intent(MainActivity.this,Registration.class));
+
+                Intent i = new Intent(MainActivity.this,Registration.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+
+
+
+}
